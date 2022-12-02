7a30827eed12e05ff27fe6b3344820e5e026fcbe
Baocode0202 <devbaopham222000@gmail.com>
2022-12-02 18:41:29 +0700

    feat: setup Route

---
+++ pages/home/header/[id].jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>BaoPham</title>
+      </Head>
+      <h1>cqbicqboiqcbiobiqibcqibbiowciboqioc</h1>
+    </div>
+  );
+}
